8250a4c1bbe51a763c0cbef08da5c4d78296c5d8
Tobit Raff <0xtobit@gmail.com>
2017-01-28 14:33:40 -0700

    Add summoner.get_new_match_ids method

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,4 +1,16 @@
 class Summoner < ActiveRecord::Base
   has_many :matches, inverse_of: :summoner
   # TODO uniqueness
+
+  def get_new_match_ids
+    client = RiotApi.client
+    summoner = client.get_summoner_by_name(name)
+
+    existing_match_ids = matches.pluck(:match_id).to_set
+    new_match_ids = Set.new
+    summoner.match_list.each do |match|
+      new_match_ids << match.match_id unless existing_match_ids.include?(match.match_id)
+    end
+    new_match_ids.to_a
+  end
 end
