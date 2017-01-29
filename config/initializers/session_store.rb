c515807ffe587ac46bf1c89f7646fda76331d238
Tobit Raff <0xtobit@gmail.com>
2017-01-28 20:18:16 -0700

    Add footer, fix some old names

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_jolk_session'
+Rails.application.config.session_store :cookie_store, key: '_deeper_dive_session'
